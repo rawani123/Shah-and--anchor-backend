85bc6338871aa08ce855fe446458674ba896e52e
Aniruddha Rawool <aniruddha.212074101@vcet.edu.in>
2024-03-06 13:03:11 +0530

    Routes

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,4 +1,4 @@
-import userModel from "../modules/user.model";
+import userModel from "../modules/user.model.js";
 import bcrypt from "bcrypt";
 export const registerController = async (req, res) => {
     try {
@@ -7,11 +7,40 @@ export const registerController = async (req, res) => {
             return res.status(400).send({message:"Please fill in all fields"});
         }
         const userExists = await userModel({email});
-        if(userExists){
+        if(!userExists){
             return res.status(400).send({message:"User already exists"});
         }
         const hashedPassword = await bcrypt.hash(password, 12);
+
+        const user = await userModel.create({
+            userName,
+            email,
+            password:hashedPassword,
+        });
+
+        return res.status(201).send({message:"User created successfully",user});
+    } catch (error) {
+        return res.status(500).send({ message: error.message });
+    }
+}
+
+export const loginController = async (req, res) => {
+    try {
+        const {email,password}=req.body;
+        if(!email || !password){
+            return res.status(400).send({message:"Please fill in all fields"});
+        }
+        const user = await userModel.findOne({email});
+        if(!user){
+            return res.status(400).send({message:"User does not exist"});
+        }
+        const passwordMatch = await bcrypt.compare(password, user.password);
+        if(!passwordMatch){
+            return res.status(400).send({message:"Invalid credentials"});
+        }
+
         
+        return res.status(200).send({message:"Login successful",user});
     } catch (error) {
         return res.status(500).send({ message: error.message });
     }
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv"
 import connectDB from "./config/db.js";
 import express from "express";
+import userRoutes from "./routes/user.routes.js";
 
 
 dotenv.config();
@@ -8,6 +9,10 @@ dotenv.config();
 const PORT = process.env.PORT;
 
 const app = express();
+
+app.use(express.json());
+
+app.use("/api/users",userRoutes)
 
 app.listen(PORT, () => {
     console.log("Server running on port 5000")
