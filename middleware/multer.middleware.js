04df8d20b089c8422d606ee637c66d7c76623943
Aniruddha Rawool <aniruddha.212074101@vcet.edu.in>
2024-03-06 15:35:15 +0530

    multer

--- middleware/multer.middleware.js
+++ middleware/multer.middleware.js
@@ -10,7 +10,6 @@ const storage = multer.diskStorage({
 })
 
 
-
 export const upload = multer({
     storage,
 });
--- routes/sponsor.routes.js
+++ routes/sponsor.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { getAllSponsors, sponsorController } from "../controllers/sponsor.controller.js";
 import { authentication } from "../middleware/auth.mid.js";
+import {upload } from "../middleware/multer.middleware.js";
 
 const router = express.Router();
 
-router.post("/become-sponsor",sponsorController)
+router.post("/become-sponsor",upload.single('video'),sponsorController)
 router.post('/getAllSponsors',authentication,getAllSponsors)
 
 export default router;
