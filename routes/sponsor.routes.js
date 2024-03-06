16d7ec378bdc06bf8b09f3af788f26a6ce7f6ff1
kashish567 <kashishbhanushali7@gmail.com>
2024-03-06 14:56:26 +0530

    update-kashish

--- routes/sponsor.routes.js
+++ routes/sponsor.routes.js
@@ -1,10 +1,14 @@
 import express from "express";
-import { getAllSponsors, sponsorController } from "../controllers/sponsor.controller.js";
+import { deleteSponsor, getAllSponsors, getSponsor, sponsorController, updateSponsor } from "../controllers/sponsor.controller.js";
 import { authentication } from "../middleware/auth.mid.js";
 
 const router = express.Router();
 
 router.post("/become-sponsor",sponsorController)
 router.post('/getAllSponsors',authentication,getAllSponsors)
+router.post('/updateSponsor', authentication,updateSponsor)
+router.post('/getSponsor',authentication,getSponsor)
+router.post('/updateSponsor',authentication,updateSponsor)
+router.post('/deleteSponsor',authentication,deleteSponsor)
 
 export default router;
