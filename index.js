495b996edc98798f127c0b2d1019a0a1dc0b2b83
Aniruddha Rawool <aniruddha.212074101@vcet.edu.in>
2024-03-06 12:39:54 +0530

    UserSchema Adde

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 import dotenv from "dotenv"
 import connectDB from "./config/db.js";
+import express from "express";
+
 
 dotenv.config();
 
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.listen(PORT, () => {
+    console.log("Server running on port 5000")
+})
+
+
 connectDB();
+
---
+++ modules/userSchema.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    userName:{
+        type:String,
+        required:true,
+    },
+    email:{
+        type:String,
+        required:true,
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    isAdmin:{
+        type:Boolean,
+        default:false,
+    },
+    isSponsor:{
+        type:Boolean,
+        default:false,
+    },
+    notifications:{
+        type:Array,
+        default:[],
+    },
+    seenNotifications:{
+        type:Array,
+        default:[],
+    },
+},{timestamps:true});
+
+const userModel= mongoose.model("User",userSchema);
+
+export default userModel;
